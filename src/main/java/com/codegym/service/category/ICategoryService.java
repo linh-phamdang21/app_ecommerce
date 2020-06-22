e7d08520fbea00ed1bbea6c7e532a89a6f4e06d8
badat <you@example.com>
2020-06-22 16:24:00 +0700

    CRUD PRODUCT and Category

---
+++ src/main/java/com/codegym/service/category/ICategoryService.java
@@ -0,0 +1,7 @@
+package com.codegym.service.category;
+
+import com.codegym.model.Category;
+import com.codegym.service.generic.IGenericService;
+
+public interface ICategoryService extends IGenericService<Category> {
+}
--- src/main/java/com/codegym/service/product/IProduceService.java
+++ src/main/java/com/codegym/service/product/IProduceService.java
@@ -2,7 +2,11 @@ package com.codegym.service.product;
 
 import com.codegym.model.Product;
 import com.codegym.service.generic.IGenericService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 public interface IProduceService extends IGenericService<Product> {
+    Page<Product> findAll(Pageable pageable);
+    Page<Product> findAllByProductNameContaining(String productName,Pageable pageable);
 }
